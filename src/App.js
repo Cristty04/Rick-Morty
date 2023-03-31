21afc20c1a9ec35996a8e0ee244d35cf88f51892
i-am-iron-man <iron-man@avengers.com>
2023-03-31 20:38:05 +0200

    fin

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Login from "./pages/Login"
 import Home from './pages/Home';
 import RequiredAuth from './components/RequiredAuth';
-import Gallery from './pages/Gallery'
+import Gallery from './pages/Gallery';
+import Location from './pages/Location';
+
+
 
 function App() {
   const [user, setUser] = useState(null) 
@@ -14,26 +17,34 @@ function App() {
  //con esta función le pasamos a la función del archivo auth.js el email y el password para que el setUser recoja el usuario encontrado
   const loginUser =({email, password}) => setUser(login({ email, password }));
   const logoutUser = () => setUser(null);
+ 
 
+  
 
   return (
-    <div className="App">
+     
       <Router>
         {/* a navigator le pasamos authenticated para que sepa cuando pintar el boton de login o logout y la función de logout para que la pueda usar*/}
-        <Navigator authenticated={authenticated} logoutUser={logoutUser}/>
-      <Routes>
+        <Navigator className="navigator" authenticated={authenticated} logoutUser={logoutUser}/>
+      <Routes class= "rutas">
         <Route path='/' element={<Home/>}/>
+        <Route path='/location' element={<Location/>}/>
         <Route path='/gallery' element={
           <RequiredAuth authenticated= {authenticated}>
             <Gallery/>
             </RequiredAuth>
-      }></Route>
-        <Route path='/login' element={<Login loginUser={loginUser}/>}/>
+      }>
+
+      </Route>
+      <Route path='/login' element={<Login loginUser={loginUser}/>}/>
       </Routes>
       </Router>
       
-    </div> 
+    
   );
-}
 
-export default App;
+};
+
+
+
+export default App
